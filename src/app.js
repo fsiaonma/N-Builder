0ebd5c795a3820b385eb778f6fddd81e2414f727
junhao <28962383@qq.com>
2013-06-24 12:51:52 +0800

    提交
    提交

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 var fs  = require('./modules/fs'); 
 
-var base = require('./core/base.js');
 var imageHandler = require('./core/handlers/imagesHandler');
 var jsHandler = require('./core/handlers/jsHandler');
 var cssHandler = require('./core/handlers/cssHandler');
@@ -11,22 +10,16 @@ var config = require('../config');
     config.map(function(item) {
         fs.mkdir(item.buildPath);
         if (item.images) {
-            var imagesFloderPath = item.buildPath + (item.images.buildPath? item.images.buildPath : '');
-            base.createFloder(imagesFloderPath, function() {
-                imageHandler.unpackImages(item.rootPath, imagesFloderPath, item.images);
-            });
-        } 
+            var buildPath = item.buildPath + (item.images.buildPath? item.images.buildPath : '');
+            imageHandler.unpackImages(item.rootPath, buildPath, item.images);
+        }
         if (item.js) {
-            var jsFloderPath = item.buildPath + (item.js.buildPath? item.js.buildPath : '');
-            base.createFloder(jsFloderPath, function() {
-                jsHandler.unpackJs(item.rootPath, jsFloderPath, item.js);
-            });
+            var buildPath = item.buildPath + (item.js.buildPath? item.js.buildPath : '');
+            jsHandler.unpackJs(item.rootPath, buildPath, item.js);
         }
         if (item.css) {
-            var cssFloderPath = item.buildPath + (item.css.buildPath? item.css.buildPath : '');
-            base.createFloder(cssFloderPath, function() {
-                cssHandler.unpackCss(item.rootPath, cssFloderPath, item.css);
-            });
+            var buildPath = item.buildPath + (item.css.buildPath? item.css.buildPath : '');
+            cssHandler.unpackCss(item.rootPath, buildPath, item.css);
         }
     });
 })();
