89a25f79520dbe3737239f652d63db089ea73133
sam <289623783@qq.com>
2014-06-06 18:15:05 +0800

    修改：增加复制压缩功能
    修改：增加复制压缩功能

--- src/core/handlers/jsHandlers/jsCopy.js
+++ src/core/handlers/jsHandlers/jsCopy.js
@@ -1,11 +1,14 @@
+var fs = require('fs'); 
 var walk = require('walk');
+var jsp = require("uglify-js").parser;
+var pro = require("uglify-js").uglify;
 
 var base = require('../../common/base');
 
 var jsCopy = (function() {
 	var copyFilesPath = [];
 
-	var doCopy = function(rootPath, buildPath, paths, devPath, path) {
+	var doCopy = function(rootPath, buildPath, paths, devPath, path, isMerge) {
         for (var i = 0, len = paths.ignore.length; i < len; ++i) {
             if (paths.ignore[i] == devPath) {
                 return ;
@@ -16,38 +19,63 @@ var jsCopy = (function() {
             copyFilesPath.push(devPath);
             var fileInPath = rootPath + path;
             var fileOutPath = buildPath + path;
-            base.copyFile(fileInPath, fileOutPath);
+            if (isMerge) {
+                doCompression(fileInPath, fileOutPath);
+            } else {
+                base.copyFile(fileInPath, fileOutPath);
+            }
         }
+    }
+
+    var doCompression = function(fileIn, fileOut) {
+        var finalCode = [];
+        var origCode = "";
+        var ast = "";
+        console.log("[compressing js] compressing " + fileIn);
+        origCode = fs.readFileSync(fileIn, 'utf8');
+        ast = jsp.parse(origCode);
+        ast = pro.ast_mangle(ast); 
+        ast = pro.ast_squeeze(ast);
+        finalCode.push(pro.gen_code(ast), ';');
+        base.createFloder(fileOut.substr(0, fileOut.lastIndexOf("/")), function(){
+            fs.writeFileSync(fileOut, finalCode.join(''), 'utf8');
+        });
     }
 
     var o = {};
 
 	o.walkForPaths = function(rootPath, buildPath, jsConfig, paths, callback) {
         var copyWalking = 0;
-        jsConfig.copyOnly.map(function(copyFilePath) {
-            (function(path) {
-                var callFunc = arguments.callee;
-                var self = this;
-                var devPath = rootPath + path;
-                if (devPath[devPath.length - 1] == '/') {
-                    ++copyWalking;
-                    var walker = walk.walk(devPath.substr(0, devPath.lastIndexOf('/')));
-                    walker.on("file", function (root, fileStats, next) {
-                        var fileRootPathName = (root[root.length - 1] == '/') ? root + fileStats.name : root + '/' + fileStats.name;
-                        var filePathName = fileRootPathName.substr(rootPath.length);
-                        callFunc.call(self, filePathName);
-                        next();
-                    });
-                    walker.on("end", function() {
-                        if (--copyWalking == 0) {
-                            callback && callback(copyFilesPath);
+
+        jsConfig.copyOnly.map(function(copyConfig) {
+            (function(config) {
+                config.dir.map(function(copyFilePath) {
+                    (function(path) {
+                        var callFunc = arguments.callee;
+                        var self = this;
+                        var devPath = rootPath + path;
+                        if (devPath[devPath.length - 1] == '/') {
+                            ++copyWalking;
+                            var walker = walk.walk(devPath.substr(0, devPath.lastIndexOf('/')));
+                            walker.on("file", function (root, fileStats, next) {
+                                var fileRootPathName = (root[root.length - 1] == '/') ? root + fileStats.name : root + '/' + fileStats.name;
+                                var filePathName = fileRootPathName.substr(rootPath.length);
+                                callFunc.call(self, filePathName);
+                                next();
+                            });
+                            walker.on("end", function() {
+                                if (--copyWalking == 0) {
+                                    callback && callback(copyFilesPath);
+                                }
+                            });
+                        } else {
+                            doCopy(rootPath, buildPath, paths, devPath, path, config.merge);
                         }
-                    });
-                } else {
-                	doCopy(rootPath, buildPath, paths, devPath, path);
-                }
-            })(copyFilePath);
+                    })(copyFilePath);
+                });
+            })(copyConfig);
         });
+
         if (copyWalking == 0) {
             callback && callback(copyFilesPath);
         }
